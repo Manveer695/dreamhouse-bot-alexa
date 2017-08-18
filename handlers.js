c381f67a1dade922dc8b65fe238e6ae8ca7fe9a3
Manveer695 <singhmanveer1995@gmail.com>
2017-08-18 15:37:06 +0530

    second commit

--- handlers.js
+++ handlers.js
@@ -1,7 +1,38 @@
 "use strict";
 
+var http = require('http');
+
 exports.callSusiApi = (slots, session, response) => {
     let query = slots.query.value;
     console.log(query);
-    response.say("OK, in what city?");
+
+    var endpoint = "http://api.susi.ai/susi/chat.json?q="+query; // ENDPOINT GOES HERE
+    var body = ""
+    https.get(endpoint, (response) => {
+      response.on('data', (chunk) => { body += chunk })
+      response.on('end', () => {
+        var data = JSON.parse(body);
+        if(data.answers[0].actions[1]){
+            if(data.answers[0].actions[1].type === 'rss'){
+                viewCount = 'I have no idea about it, sorry.';
+            }
+        }
+        else{
+            if(data.answers[0].actions[0].type === 'table'){
+                var colNames = data.answers[0].actions[0].columns;
+                viewCount = '';
+                if((data.answers[0].metadata.count)>10)
+                    viewCount += 'Due to message limit, only some results are spoke. They are: ';
+                for(var i=0;i<(((data.answers[0].metadata.count)>10)?10:data.answers[0].metadata.count);i++){
+                    viewCount += data.answers[0].data[i].name+',';
+                }
+            }
+            else
+            {
+                viewCount = data.answers[0].actions[0].expression;
+            }
+        }
+        response.say(viewCount);
+      })
+    })
 };
